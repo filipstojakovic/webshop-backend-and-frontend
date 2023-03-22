06270608e73c6cd7ff1ce23a425cfb8757ec7302
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-03-22 17:52:35 +0100

    Initial commit

---
+++ src/test/java/org/etf/webshopbackend/WebshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.etf.webshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
