d6523ce51e350e0da3061902dccd959e0e6318ca
Filip Stojakovic <filip.stojakovic1@gmail.com>
2023-06-06 12:31:49 +0200

    attribute specificaiton working?

--- src/main/java/org/etf/webshopbackend/specification/ProductSpecifications.java
+++ src/main/java/org/etf/webshopbackend/specification/ProductSpecifications.java
@@ -1,9 +1,11 @@
 package org.etf.webshopbackend.specification;
 
-import org.etf.webshopbackend.model.entity.Attribute_;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+import org.etf.webshopbackend.model.entity.Attribute;
 import org.etf.webshopbackend.model.entity.Category_;
 import org.etf.webshopbackend.model.entity.Product;
-import org.etf.webshopbackend.model.entity.ProductHasAttribute_;
+import org.etf.webshopbackend.model.entity.ProductHasAttribute;
 import org.etf.webshopbackend.model.entity.Product_;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -12,15 +14,18 @@ import java.util.Objects;
 public class ProductSpecifications {
 
   public static Specification<Product> productByName(String name) {
-    return (root, query, criteriaBuilder) -> {
-      if (Objects.isNull(name)) {
-        return criteriaBuilder.and(); // it's like empty criteria
-      }
-      return criteriaBuilder.like(
-          criteriaBuilder.lower(root.get(Product_.NAME)),
-          "%" + name.toLowerCase() + "%"
-      );
-    };
+    if (Objects.isNull(name)) {
+      return emptySpecification();
+    }
+
+    return (root, query, criteriaBuilder) -> criteriaBuilder.like(
+        criteriaBuilder.lower(root.get(Product_.NAME)),
+        "%" + name.toLowerCase() + "%"
+    );
+  }
+
+  public static Specification<Product> emptySpecification() {
+    return (root, query, criteriaBuilder) -> criteriaBuilder.and(); // it's like empty criteria
   }
 
   public static Specification<Product> notPurchased() {
@@ -28,6 +33,9 @@ public class ProductSpecifications {
   }
 
   public static Specification<Product> byCategoryName(String categoryName) { // should use categoryId
+    if (Objects.isNull(categoryName)) {
+      return emptySpecification();
+    }
     return (root, query, criteriaBuilder) -> {
       // Join the category entity
       root.join(Product_.CATEGORY);
@@ -38,34 +46,15 @@ public class ProductSpecifications {
     };
   }
 
-  public static Specification<Product> byAttributeNameAndValue(String attributeName, String attributeValue) {
-    Specification<Product> byAttributeNameSpec = byAttributeName(attributeName);
-    Specification<Product> byAttributeValueSpec = byAttributeValue(attributeValue);
-
-    return byAttributeNameSpec.and(byAttributeValueSpec);
-  }
-
-  private static Specification<Product> byAttributeName(String attributeName) {
+  public static Specification<Product> hasAttributeWithValue(String attributeName, String attributeValue) {
     return (root, query, criteriaBuilder) -> {
-      // Join the productHasAttributes and attributes entities
-      root.join(Product_.PRODUCT_HAS_ATTRIBUTES);
-      root.join(Product_.PRODUCT_HAS_ATTRIBUTES).join(ProductHasAttribute_.ATTRIBUTE);
+      Join<Product, ProductHasAttribute> join = root.join("productHasAttributes");
+      Join<ProductHasAttribute, Attribute> attributeJoin = join.join("attribute");
 
-      return criteriaBuilder.like(
-          criteriaBuilder.lower(root.get(Product_.PRODUCT_HAS_ATTRIBUTES).get(ProductHasAttribute_.ATTRIBUTE)
-              .get(Attribute_.NAME)), "%" + attributeName.toLowerCase() + "%"
-      );
-    };
-  }
+      Predicate attributePredicate = criteriaBuilder.equal(attributeJoin.get("name"), attributeName);
+      Predicate valuePredicate = criteriaBuilder.equal(join.get("value"), attributeValue);
 
-  private static Specification<Product> byAttributeValue(String attributeValue) {
-    return (root, query, criteriaBuilder) -> {
-      // Join the productHasAttributes entity
-      root.join(Product_.PRODUCT_HAS_ATTRIBUTES);
-
-      return criteriaBuilder.like(
-          criteriaBuilder.lower(root.get(Product_.PRODUCT_HAS_ATTRIBUTES).get(ProductHasAttribute_.VALUE)), "%" + attributeValue.toLowerCase() + "%"
-      );
+      return criteriaBuilder.and(attributePredicate, valuePredicate);
     };
   }
 
